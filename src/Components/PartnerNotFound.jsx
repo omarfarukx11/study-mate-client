f91de5ad34a37ced40b084048a5088affe71b2c2
Omar Faruk <khfarukcht@mail.com>
2025-11-16 09:12:35 +0600

    dynaimic title add

--- src/Components/PartnerNotFound.jsx
+++ src/Components/PartnerNotFound.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 const PartnerNotFound = () => {
     return (
         <div className='flex items-center justify-center flex-col '>
+            <title>StudyMate - Not Found</title>
             <img className='mx-auto w-[30%]' src="https://i.ibb.co.com/JjntHKkn/404-error-with-cute-animal-concept-illustration-114360-1900.jpg" alt="" />
 
             <Link to={'/'} className="w-full btn md:w-auto border-2 border-[#5BBC2E] bg-white text-[#5BBC2E] font-semibold px-4 py-2 rounded-sm shadow-md transition-all duration-300 hover:bg-[#5BBC2E] hover:text-white text-sm md:text-base"
